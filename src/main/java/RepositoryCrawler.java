5e50dd4b6c0830d44f37778ff28fb223fb2310f3
Yuta Tokitake <toki211@gmail.com>
2014-11-26 14:51:12 +0900

    update dependency

---
+++ src/main/java/RepositoryCrawler.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tokitake on 2014/10/02.
+ */
+public class RepositoryCrawler {
+
+    private static final String REPOPATH =
+            (new File("").getAbsolutePath().replace("pattern-detection", "camel")) + File.separator + "diff_root";
+    private static List<File> filelist;
+
+    public static void Crawl() {
+        System.out.println(REPOPATH);
+        File dir = new File(REPOPATH);
+        filelist = new ArrayList<File>();
+        readDirectory(dir);
+    }
+
+    public static List<File> getFilelist() {
+        return filelist;
+    }
+
+    private static void readDirectory(File dir) {
+        List<File> files = java.util.Arrays.asList(dir.listFiles());
+        for (File file : files) {
+            if (!file.exists()) {
+                continue;
+            } else if (file.isDirectory()) {
+                readDirectory(file);
+            } else if (file.isFile() && file.getName().endsWith(".java")) {
+                filelist.add(file);
+            }
+        }
+    }
+
+
+}

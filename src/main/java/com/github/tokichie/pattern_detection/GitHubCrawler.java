90075d00f9bc31cc6d20438d3fe17e6261f7cce7
Yuta Tokitake <toki211@gmail.com>
2014-12-08 00:07:04 +0900

    Now GitHub credentials are read from either json file or environment variables

--- src/main/java/com/github/tokichie/pattern_detection/GitHubCrawler.java
+++ src/main/java/com/github/tokichie/pattern_detection/GitHubCrawler.java
@@ -3,6 +3,7 @@ package com.github.tokichie.pattern_detection;
 
 import com.google.common.io.Resources;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.kohsuke.github.GHIssue;
@@ -14,8 +15,11 @@ import org.kohsuke.github.GitHub;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tokitake on 2014/12/05.
@@ -24,21 +28,26 @@ public class GitHubCrawler {
 
   public static void getIssueComments(File repositoryListCsv, int limit) {
     try {
-      String authInfoJson = Resources.toString(Resources.getResource("AuthInfo.json"), StandardCharsets.UTF_8);
-      GitHubAuthInfo gitHubAuthInfo = new ObjectMapper().readValue(authInfoJson, GitHubAuthInfo.class);
+      Map<String, String> env = System.getenv();
+      if (! (env.containsKey("login") && env.containsKey("token"))) {
+        String authInfoJson =
+            Resources.toString(Resources.getResource("AuthInfo.json"), StandardCharsets.UTF_8);
+        env = new ObjectMapper().readValue(authInfoJson, new TypeReference<HashMap<String, String>>(){});
+      }
 
-      GitHub github = GitHub.connect(gitHubAuthInfo.login, gitHubAuthInfo.token);
+      GitHub github = GitHub.connect(env.get("login"), env.get("token"));
       GHRepository repo = github.getRepository("tokichie/pattern-detection");
       List<GHIssue> closedIssues = repo.getIssues(GHIssueState.CLOSED);
-      List<GHPullRequest> closedPullReqs = repo.getPullRequests(GHIssueState.CLOSED);
 
       System.out.println(closedIssues.size() + " issues");
       for (GHIssue issue : closedIssues) {
-        if (issue.getCommentsCount() > 0) {
+        if (issue.getCommentsCount() > 0 && issue.getNumber() == 11) {
+          System.out.println("issue #" + issue.getNumber());
           List<GHIssueComment> comments = issue.getComments();
           for (GHIssueComment comment : comments) {
-            System.out.println(comment.getBody());
+            System.out.println("\t" + comment.getBody() + "\n");
           }
+
         }
       }
     } catch (IOException e) {

20646b35b97f7f218d19e1895de50782ba658a69
Yuta Tokitake <toki211@gmail.com>
2014-12-17 11:03:59 +0900

    fix commands of git_archive.sh

---
+++ src/main/java/com/github/tokichie/pattern_detection/GitHubDiff.java
@@ -0,0 +1,20 @@
+package com.github.tokichie.pattern_detection;
+
+import com.jcabi.github.Repo;
+
+import java.util.List;
+
+/**
+ * Created by tokitake on 2014/12/16.
+ */
+public class GitHubDiff {
+  private RepositoryInfo repoInfo;
+
+  public GitHubDiff(RepositoryInfo repoInfo) {
+    this.repoInfo = repoInfo;
+  }
+
+  public void takeDiff() {
+
+  }
+}
